3aa8bffc842391cdf6e4a5526eceb2a16c04ebd0
LunarArray <lunarArray@proton.me>
2024-11-17 18:55:21 +0100

    simpler lsp

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -1,121 +1,22 @@
-{ helpers, pkgs, ... }:
-#
-let
-  thunk =
-    body:
-    helpers.mkRaw ''
-      function()
-        ${body}
-      end
-    '';
-in
+{pkgs, ...}:
 {
-  extraPackages = with pkgs; [ nixfmt-rfc-style ];
-
-  extraConfigLuaPre = ''
-    do
-      local diagnostic_signs = { Error = "", Warn = "", Hint = "", Info = "" }
-
-      for type, icon in pairs(diagnostic_signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-      end
-
-      vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-        underline = true,
-        update_in_insert = true,
-        virtual_text = { spacing = 4, prefix = "●" },
-        severity_sort = true,
-      })
-    end
-  '';
-
-  autoCmd = [
-    {
-      event = [
-        "BufRead"
-        "BufNewFile"
-      ];
-      pattern = [
-        "*.txt"
-        "*.md"
-        "*.tex"
-        "LICENSE"
-      ];
-      command = "setlocal spell";
-    }
-  ];
-
-  plugins.lsp = {
-    enable = true;
-    inlayHints = true;
-
-    keymaps = {
-      silent = true;
-
-      diagnostic = {
-        "<leader>j" = "goto_next";
-        "<leader>k" = "goto_prev";
-      };
-
-      lspBuf = {
-        K = "hover";
-        gh = "hover";
-        gD = "references";
-        # gd = "definition";
-        gi = "implementation";
-        gt = "type_definition";
-      };
-
-      extra = [
-        {
-          action = thunk "vim.cmd [[<cmd>LspStop<CR>]]";
-          key = "<leader>lx";
-          options = {
-            desc = "LSP: Stop";
-            silent = true;
-          };
-        }
-        {
-          action = thunk "vim.cmd [[<cmd>LspStart<CR>]]";
-          key = "<leader>ls";
-          options = {
-            desc = "LSP: Start";
-            silent = true;
-          };
-        }
-        {
-          action = thunk "vim.cmd [[<cmd>LspRestart<CR>]]";
-          key = "<leader>lr";
-          options = {
-            desc = "LSP: Restart";
-            silent = true;
-          };
-        }
-        {
-          action =
-            helpers.mkRaw
-              # lua
-              ''
-                require('telescope.builtin').lsp_definitions
-              '';
-          key = "<leader>gd";
-          options = {
-            desc = "LSP: Definitions";
-            silent = true;
-          };
-        }
-      ];
-    };
-
-    servers = {
-      cmake.enable = true;
-      cssls.enable = true;
-      html.enable = true;
-      jsonls.enable = true;
-      ltex.enable = true;
-      lua-ls.enable = true;
-      nixd = {
+	plugins = {
+		cmp ={
+  		autoEnableSources = true;
+  		settings.sources = [
+    		{ name = "nvim_lsp"; }
+    		{ name = "path"; }
+    		{ name = "buffer"; }
+  			];
+			};
+		rustaceanvim.enable = true;
+		trouble.enable = true;
+		lsp = {
+			enable = true;
+			servers = {
+			elixirls.enable = true;
+			bashls.enable = true;
+			nixd = {
         enable = true;
 
         settings = {
@@ -136,14 +37,24 @@ in
                 let configs = (builtins.getFlake ("git+file://" + builtins.toString ./.)).homeConfigurations; in (builtins.head (builtins.attrValues configs)).options
               '';
             };
+            darwin = {
+              expr = ''
+                let configs = (builtins.getFlake ("git+file://" + builtins.toString ./.)).darwinConfigurations; in (builtins.head (builtins.attrValues configs)).options
+              '';
+            };
           };
         };
       };
-      rust-analyzer = {
-        enable = true;
-        installCargo = true;
-        installRustc = true;
-      };
-    };
-  };
+			marksman.enable = true;
+			ltex = {
+				enable = true;
+				settings.language = "de-DE";
+				};
+			java_language_server.enable = true;
+			#servers
+			};
+		#lsp
+		};
+	#plugins
+	};
 }
